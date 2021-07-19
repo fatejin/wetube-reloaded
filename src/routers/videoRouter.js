6331f9f16fb7cc7cfcc9ca191555cf0353fd7772
fatejin <where9810@gmail.com>
2021-07-19 20:43:40 +0900

    #6.10 Video Model(video schema_Tracking Errors on DB)

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -5,14 +5,12 @@ import {
   getEdit,
   postEdit,
   postUpload,
-  deleteVideo,
 } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
-videoRouter.get("/:id([0-9a-f]{24})", watch);
-videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
-videoRouter.route("/:id([0-9a-f]{24})/delete").get(deleteVideo);
+videoRouter.get("/:id(\\d+)", watch);
+videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
 videoRouter.route("/upload").get(getUpload).post(postUpload);
 
 export default videoRouter;
