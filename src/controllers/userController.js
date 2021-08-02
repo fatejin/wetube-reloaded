c02b155af95bc9e576c2d9ade60c433d0c1dd6fb
fatejin <where9810@gmail.com>
2021-08-02 12:53:41 +0900

    #4.6 Planning Routes(Fixing Bug)

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,3 +1,6 @@
 export const join = (req, res) => res.send("Join");
 export const watch = (req, res) => res.send("Watch");
 export const remove = (req, res) => res.semd("Remove User");
+export const login = (req, res) => res.send("Login");
+export const logout = (req, res) => res.send("Log out");
+export const see = (req, res) => res.send("See User");
