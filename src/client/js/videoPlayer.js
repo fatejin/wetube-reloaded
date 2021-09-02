b4bbbc514663e4e13a31d3d3ae68c77f21243635
fatejin <where9810@gmail.com>
2021-09-02 15:53:02 +0900

    #11.1 Play Pause ~ #11.2 Mute and Unmute Error fixed (2021/09/02)

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -4,20 +4,30 @@ const muteBtn = document.getElementById("mute");
 const time = document.getElementById("time");
 const volume = document.getElementById("volume");
 
-const handlePlayClick = (e) => {
-  console.log(video);
+const handlePlayClick = () => {
   if (video.paused) {
     video.play();
   } else {
     video.pause();
   }
 };
+
 const handlePause = () => (playBtn.innerText = "Play");
+
 const handlePlay = () => (playBtn.innerText = "Pause");
 
-const handleMute = (e) => {};
+const handleMute = (e) => {
+  if (video.muted) {
+    video.muted = false;
+    muteBtn.innerText = "Mute";
+  } else {
+    video.muted = true;
+    muteBtn.innerText = "Unmute";
+  }
+};
 
 playBtn.addEventListener("click", handlePlayClick);
-muteBtn.addEventListener("click", handleMute);
 video.addEventListener("pause", handlePause);
 video.addEventListener("play", handlePlay);
+
+muteBtn.addEventListener("click", handleMute);
