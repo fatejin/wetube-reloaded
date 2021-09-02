43e2bf5f6805f3d5c9b91d0e5e62c7c900303788
fatejin <where9810@gmail.com>
2021-09-02 16:16:14 +0900

    #11.2 Mute and Unmute(Lazy programmer -mute EventListener Errors (2021/09/02)

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -2,32 +2,26 @@ const video = document.querySelector("video");
 const playBtn = document.getElementById("play");
 const muteBtn = document.getElementById("mute");
 const time = document.getElementById("time");
-const volume = document.getElementById("volume");
+const volumeRange = document.getElementById("volume");
 
-const handlePlayClick = () => {
+const handlePlayClick = (e) => {
   if (video.paused) {
     video.play();
   } else {
     video.pause();
   }
+  playBtn.innerText = video.paused ? "Play" : "Pause";
 };
-
-const handlePause = () => (playBtn.innerText = "Play");
-
-const handlePlay = () => (playBtn.innerText = "Pause");
 
 const handleMute = (e) => {
   if (video.muted) {
     video.muted = false;
-    muteBtn.innerText = "Mute";
   } else {
     video.muted = true;
-    muteBtn.innerText = "Unmute";
+    muteBtn.innerText = video.muted ? "UnMute" : "Mute";
+    volumeRange.value = video.muted ? 0 : 0.5;
   }
 };
 
 playBtn.addEventListener("click", handlePlayClick);
-video.addEventListener("pause", handlePause);
-video.addEventListener("play", handlePlay);
-
 muteBtn.addEventListener("click", handleMute);
