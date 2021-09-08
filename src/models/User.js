d7f3a50bf542e8dbc6facb7983b48d12bf0bac8a
fatejin <where9810@gmail.com>
2021-09-08 13:35:34 +0900

    #16.1 Comment Models(2021/09/07)

--- src/models/User.js
+++ src/models/User.js
@@ -1,6 +1,5 @@
 import bcrypt from "bcrypt";
 import mongoose from "mongoose";
-
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   avatarUrl: String,
@@ -9,6 +8,7 @@ const userSchema = new mongoose.Schema({
   password: { type: String },
   name: { type: String, required: true },
   location: String,
+  comments: [{ type: mongoose.Schema.Types.ObjectId, ref: "Comment" }],
   videos: [{ type: mongoose.Schema.Types.ObjectId, ref: "Video" }],
 });
 
@@ -17,6 +17,5 @@ userSchema.pre("save", async function () {
     this.password = await bcrypt.hash(this.password, 5);
   }
 });
-
 const User = mongoose.model("User", userSchema);
 export default User;
