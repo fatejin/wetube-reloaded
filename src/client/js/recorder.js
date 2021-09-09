1c09c77df45e904462aaab07362dcabe30b73e3f
fatejin <where9810@gmail.com>
2021-09-09 23:49:45 +0900

    #13.4 Downloading the File_Fixing_Bug(2021/09/09)

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -3,8 +3,15 @@ const video = document.getElementById("preview");
 
 let stream;
 let recorder;
+let videoFile;
 
-const handleDownload = () => {};
+const handleDownload = () => {
+  const a = document.createElement("a");
+  a.href = videoFile;
+  a.download = "MyRecording.webm";
+  document.body.appendChild(a);
+  a.click();
+};
 
 const handleStop = () => {
   startBtn.innerText = "Download Recording";
@@ -12,14 +19,13 @@ const handleStop = () => {
   startBtn.addEventListener("click", handleDownload);
   recorder.stop();
 };
-
 const handleStart = () => {
   startBtn.innerText = "Stop Recording";
   startBtn.removeEventListener("click", handleStart);
   startBtn.addEventListener("click", handleStop);
-  recorder = new MediaRecorder(stream);
+  recorder = new MediaRecorder(stream, { mimeType: "video/webm" });
   recorder.ondataavailable = (event) => {
-    const videoFile = URL.createObjectURL(event.data);
+    videoFile = URL.createObjectURL(event.data);
     video.srcObject = null;
     video.src = videoFile;
     video.loop = true;
@@ -27,7 +33,6 @@ const handleStart = () => {
   };
   recorder.start();
 };
-
 const init = async () => {
   stream = await navigator.mediaDevices.getUserMedia({
     audio: false,
@@ -36,7 +41,5 @@ const init = async () => {
   video.srcObject = stream;
   video.play();
 };
-
 init();
-
 startBtn.addEventListener("click", handleStart);
