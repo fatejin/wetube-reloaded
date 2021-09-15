dc8ba02be79d2a31f73bb7296b4e70bb11ef47e4
fatejin <where9810@gmail.com>
2021-09-15 10:42:59 +0900

    #16.3 API Route part One(2021/09/15)

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -1,12 +1,18 @@
 const videoContainer = document.getElementById("videoContainer");
 const form = document.getElementById("commentForm");
-const textarea = form.querySelector("textarea");
-const btn = form.querySelector("button");
 
 const handleSubmit = (event) => {
   event.preventDefault();
+  const textarea = form.querySelector("textarea");
   const text = textarea.value;
-  const video = videoContainer.dataset.id;
+  const videoId = videoContainer.dataset.id;
+  fetch(`/api/videos/${videoId}/comment`, {
+    method: "POST",
+    body: {
+      text,
+    },
+  });
 };
-
-btn.addEventListener("click", handleSubmit);
+if (form) {
+  form.addEventListener("click", handleSubmit);
+}
